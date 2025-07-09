da2e900a0911818fa6217841a84be0b8b95e05d5
Meklit Anteneh <meklitanteneh58@gmail.com>
2025-07-08 22:22:05 -0700

    the final update in the user interface

--- src/index.js
+++ src/index.js
@@ -19,7 +19,11 @@ let instructionsInput=document.querySelector("#user-instructions");
   let prompt= `User instructions: Generate a French poem about ${instructionsInput.value}`;
   let context= "You are a romantic poem expert who generates a 4-line love poem formatted using basic HTML. Do not wrap the response inside a code block.Separate each line using a <br />.At the end of the poem, sign with:<strong>Meklit's AI</strong>.Just output the HTML content — no explanation, no code block, and no title.";
   let apiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-console.log("poem generating");
+let poemElement=document.querySelector("#poem");
+poemElement.classList.remove("hidden");
+poemElement.innerHTML=`<div class="generating">⏳Generating a French poem about ${instructionsInput.value}...</div>`;
+
+  console.log("poem generating");
   console.log(`prompt: ${prompt}`);
 console.log(`context : ${context}`);
   axios.get(apiUrl).then(displayPoem);
